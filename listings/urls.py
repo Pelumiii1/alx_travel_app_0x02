3f19589460b8c0163e86628a05bed602fa069a76
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-31 23:33:12 +0100

    API Development for Listings and Bookings in Django

---
+++ listings/urls.py
@@ -0,0 +1,12 @@
+# listings/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet, basename='listings')
+router.register(r'bookings', BookingViewSet, basename='bookings')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
